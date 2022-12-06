33c686e80d7a6f2a1a20967479013caacdeeff53
Danys24 <danisserna99@gmail.com>
2022-12-06 14:49:59 -0500

    ejercico prueba spring heroku app

---
+++ src/main/java/com/example/ejercicioSwagger/EjercicioSwaggerApplication.java
@@ -0,0 +1,15 @@
+package com.example.ejercicioSwagger;
+
+import com.example.ejercicioSwagger.controller.HelloController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioSwaggerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EjercicioSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejercicioSwagger/EjercicioSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioSwagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
